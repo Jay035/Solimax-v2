ce41884c9a69a90d8d21a1dea4b3afde52de0d91
Chibuike Ukandu <codexjay08@gmail.com>
2023-08-09 16:21:21 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,24 @@ export default function Home() {
     <section className="mt-5 pl-[1.69rem] pr-20">
       <LatestPools />
       <div className="mt-8 text-white">
-        <h1 className=" text-[2.5rem] tracking-[-0.075rem] leading-[3.375rem]">The <span className="bg-gradient-linear bg-clip-text text-transparent">launchpad protocol</span> for everyone</h1>
-        <p className="text-[#D1D1D6] mb-8 tracking-[-0.01125rem] text-[1.125rem]">Our easy to use platform makes creation of your own tokens and token sales seamless. Tokens created on SoliMax will be verified and published on explorer websites</p>
+        <h1 className=" text-[2.5rem] tracking-[-0.075rem] leading-[3.375rem]">
+          The{" "}
+          <span className="bg-gradient-linear bg-clip-text text-transparent">
+            launchpad protocol
+          </span>{" "}
+          for everyone
+        </h1>
+        <p className="text-[#D1D1D6] mb-8 tracking-[-0.01125rem] text-[1.125rem]">
+          Our easy to use platform makes creation of your own tokens and token
+          sales seamless. Tokens created on SoliMax will be verified and
+          published on explorer websites
+        </p>
+        <div className="">
+          <button className="mr-8 rounded-[0.625rem] py-[1.3125rem] px-[2.4375rem] bg-[#454FDA] text-white">
+            Create new
+          </button>
+          <button>Learn more</button>
+        </div>
       </div>
     </section>
   );
