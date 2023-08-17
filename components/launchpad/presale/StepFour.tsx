b368cfc9cd43662e1281d7238f335c4ace2c0580
Chibuike Ukandu <codexjay08@gmail.com>
2023-08-17 08:41:01 +0100

    update on step four of create launchpad page

--- components/launchpad/presale/StepFour.tsx
+++ components/launchpad/presale/StepFour.tsx
@@ -21,8 +21,122 @@ export default function StepFour({ error }: FormProps) {
         </div>
         {/* Your information */}
         <div className="">
-          <p className="">Your information</p>
+          <p className="mb-[0.62rem]">Your information</p>
+          <div className="bg-[#26272B] border border-[#D1D1D6] rounded-[0.625rem] flex flex-col gap-[0.62rem] py-[0.875rem] px-[1.1875rem]">
+            <div className="flex items-center justify-between">
+              <p className="text-[0.875rem] tracking-[-0.00875rem] text-[#D1D1D6] ">
+                Total token
+              </p>
+              <p className="text-[#E4E4E7] text-[0.875rem] tracking-[-0.00875rem]">
+                145,342 SLM
+              </p>
+            </div>
+            <div className="flex items-center justify-between">
+              <p className="text-[0.875rem] tracking-[-0.00875rem] text-[#D1D1D6] ">
+                Total token
+              </p>
+              <p className="text-[#E4E4E7] text-[0.875rem] tracking-[-0.00875rem]">
+                145,342 SLM
+              </p>
+            </div>
+            <div className="flex items-center justify-between">
+              <p className="text-[0.875rem] tracking-[-0.00875rem] text-[#D1D1D6] ">
+                Total token
+              </p>
+              <p className="text-[#E4E4E7] text-[0.875rem] tracking-[-0.00875rem]">
+                145,342 SLM
+              </p>
+            </div>
+            <div className="flex items-center justify-between">
+              <p className="text-[0.875rem] tracking-[-0.00875rem] text-[#D1D1D6] ">
+                Total token
+              </p>
+              <p className="text-[#E4E4E7] text-[0.875rem] tracking-[-0.00875rem]">
+                145,342 SLM
+              </p>
+            </div>
+            <div className="flex items-center justify-between">
+              <p className="text-[0.875rem] tracking-[-0.00875rem] text-[#D1D1D6] ">
+                Total token
+              </p>
+              <p className="text-[#E4E4E7] text-[0.875rem] tracking-[-0.00875rem]">
+                145,342 SLM
+              </p>
+            </div>
+            <div className="flex items-center justify-between">
+              <p className="text-[0.875rem] tracking-[-0.00875rem] text-[#D1D1D6] ">
+                Total token
+              </p>
+              <p className="text-[#E4E4E7] text-[0.875rem] tracking-[-0.00875rem]">
+                145,342 SLM
+              </p>
+            </div>
+            <div className="flex items-center justify-between">
+              <p className="text-[0.875rem] tracking-[-0.00875rem] text-[#D1D1D6] ">
+                Total token
+              </p>
+              <p className="text-[#E4E4E7] text-[0.875rem] tracking-[-0.00875rem]">
+                145,342 SLM
+              </p>
+            </div>
+            <div className="flex items-center justify-between">
+              <p className="text-[0.875rem] tracking-[-0.00875rem] text-[#D1D1D6] ">
+                Total token
+              </p>
+              <p className="text-[#E4E4E7] text-[0.875rem] tracking-[-0.00875rem]">
+                145,342 SLM
+              </p>
+            </div>
+            <div className="flex items-center justify-between">
+              <p className="text-[0.875rem] tracking-[-0.00875rem] text-[#D1D1D6] ">
+                Total token
+              </p>
+              <p className="text-[#E4E4E7] text-[0.875rem] tracking-[-0.00875rem]">
+                145,342 SLM
+              </p>
+            </div>
+            <div className="flex items-center justify-between">
+              <p className="text-[0.875rem] tracking-[-0.00875rem] text-[#D1D1D6] ">
+                Total token
+              </p>
+              <p className="text-[#E4E4E7] text-[0.875rem] tracking-[-0.00875rem]">
+                145,342 SLM
+              </p>
+            </div>
+            <div className="flex items-center justify-between">
+              <p className="text-[0.875rem] tracking-[-0.00875rem] text-[#D1D1D6] ">
+                Total token
+              </p>
+              <p className="text-[#E4E4E7] text-[0.875rem] tracking-[-0.00875rem]">
+                145,342 SLM
+              </p>
+            </div>
+            <div className="flex items-center justify-between">
+              <p className="text-[0.875rem] tracking-[-0.00875rem] text-[#D1D1D6] ">
+                Total token
+              </p>
+              <p className="text-[#E4E4E7] text-[0.875rem] tracking-[-0.00875rem]">
+                145,342 SLM
+              </p>
+            </div>
+          </div>
         </div>
+        <p className="py-[0.88rem] px-[1.19rem] border border-[#A4D0F2] rounded-[0.625rem] text-[#F4F4F5] text-[0.875rem] bg-[#14221e]/[0.10]">
+          Please exclude{" "}
+          <span className="text-[#A4D0F2] break-all">
+            Solimax Launchpad address 0x56b153049AE32C6537afEa4B1F075889485c5609{" "}
+          </span>
+          from fees, rewards, max tx amount to start creating pools
+        </p>
+        <p className="py-[0.88rem] px-[1.19rem] border border-[#A4D0F2] rounded-[0.625rem] text-[#F4F4F5] text-[0.875rem] bg-[#14221e]/[0.10]">
+          For tokens with{" "}
+          <span className="text-[#A4D0F2] break-all">
+            burns, rebase or other special transfers,{" "}
+          </span>
+          please ensure that you have a way to whitelist multiple addresses or
+          turn off the special transfer events (By setting fees to 0 for example
+          for the duration of the presale)
+        </p>
       </form>
     </section>
   );
